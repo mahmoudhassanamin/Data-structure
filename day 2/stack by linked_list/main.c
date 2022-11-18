49de9a8d3b2fa8584a1cd3b311647b5c51777f78
mahmoud <mahmoudhassanamin123@gmail.com>
2022-11-18 21:07:08 +0200

    stack by linked_list

---
+++ day 2/stack by linked_list/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stackbyLL.h"
+int main()
+{   int data=0;
+    push(10);
+    push(20);
+    push(30);
+    int back=pop(&data);
+    if (back==0){
+        printf("stack is empty");
+        // do any thing if is empty
+    }
+    else {
+        printf("data is %d",data);
+        //do any thing here if stack return a value
+    }
+
+    return 0;
+}
